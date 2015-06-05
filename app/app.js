8af06b636f810b7496920261420b16584fd39abe
Alex Kates <daniel.alexander.kates@gmail.com>
2015-06-05 15:09:44 -0400

    initial commit

---
+++ app/app.js
@@ -0,0 +1,2 @@
+var controllers = angular.module("controllers", []);
+var app = angular.module("app", ["controllers", "firebase"]);
---
+++ app/controllers/games.js
@@ -0,0 +1,3 @@
+angular.module("controllers").controller("GamesController", function($scope) {
+    $scope.test = "HI";
+});
